92a3ac6fbe2941f26dd53a5387ccb7d4c63f993f
LeoMcBills <leokinyera9@gmail.com>
2023-05-26 22:41:08 +0300

    more with pep484_type_annotations

---
+++ documentation/doc1.py
@@ -0,0 +1,80 @@
+"""
+This directory is for documentation.
+
+This module defines functions for adding and multiplying numbers.
+"""
+
+def add(a, b):
+    """
+    Add two numbers.
+    
+    Parameters
+    ----------
+    a : float
+        First number to add.
+    b : float
+        Second number to add
+        
+    Returns
+    -------
+    float
+        Sum of the two numbers.
+    """
+    return a + b
+
+
+def multiply(a, b):
+    """
+    Multiply two numbers.
+    
+    Parameters
+    ----------
+    a : float
+        First number to multiply.
+    b : float
+        Second number to multiply
+    
+    Returns
+    -------
+    float
+        Product of the two numbers.
+    """
+    return a * b
+
+# a function  that divides two numbers
+def divide(a: float, b: float) -> float:
+    """
+    Divide two numbers.
+    
+    Parameters
+    ----------
+    a
+        First number to divide.
+    b
+        Second number to divide
+    
+    Returns
+    -------
+    float
+        Quotient of the two numbers.
+    """
+    return a / b
+
+# a function that subtracts two numbers
+def subtract(a, b):
+    """
+    Subtract two numbers.
+    
+    Parameters
+    ----------
+    a : float
+        First number to subtract.
+    b : float
+        Second number to subtract
+    
+    Returns
+    -------
+    float
+        Difference of the two numbers.
+    """
+    return a - b
