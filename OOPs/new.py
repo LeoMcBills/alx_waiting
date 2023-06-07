d75d3a3775dedf5fe996534f04e0b346bba36da7
LeoMcBills <leokinyera9@gmail.com>
2023-06-07 09:06:43 +0300

    update

--- OOPs/new.py
+++ OOPs/new.py
@@ -2,16 +2,22 @@ import kivy
 from kivy.uix.image import Image
 from kivy.uix.widget import Widget
 from kivy.app import App
-from kivy.properties import Property, NemericalProperty, ReferenceListProperty
 from kivy.uix.scatter import Scatter
-from kivy.uix.float import FloatLayout
+from kivy.uix.floatlayout import FloatLayout
+from kivy.core.window import Window
 
 
 class Dragger(App):
     def build(self):
+        Window.clearcolor = (1, 0.5, 0.5, 1)
         s = Scatter()
         f = FloatLayout()
         i = Image(source='lena.png', size_hint=(.1, .1), pos=(0, 0))
         f.add_widget(i)
         s.add_widget(f)
         return s
+    
+if __name__ == '__main__':
+    Dragger().run()
+
+
