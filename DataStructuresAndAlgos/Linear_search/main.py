0f03a30c5f7da2f8bb12a84c31cb410837d40986
LeoMcBills <leokinyera9@gmail.com>
2023-06-09 11:06:10 +0300

    choose 5 from index 3

--- DataStructuresAndAlgos/Linear_search/main.py
+++ DataStructuresAndAlgos/Linear_search/main.py
@@ -3,5 +3,9 @@
 cards = [3, 1, 6, 5, 4, 2, 7]
 choice = 5
 
-def pick(cards):
-	pass
+def pick(cards, selected):
+	for index, choice in enumerate(cards):
+		if choice == selected:
+			print(index)
+
+pick(cards, choice)
