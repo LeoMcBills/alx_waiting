0cf265719df4300a78e75d64dc4b4ce85c62c8ad
LeoMcBills <leokinyera9@gmail.com>
2023-06-10 10:17:56 +0300

    divide

--- lambda/lambda.py
+++ lambda/lambda.py
@@ -1,7 +1,11 @@
+# add
 sum = lambda x, y: x + y
-
 print(sum(3, 4))
 
+# multiply
 multiply = lambda a, b: a * b
-
 print(multiply(3, 4))
+
+# divide
+divide = lambda c, d: c / d
+print(divide(12, 4))
