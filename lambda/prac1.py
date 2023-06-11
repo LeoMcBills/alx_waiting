72795d19fb1a3b88126de36b28868cedda9dadc6
LeoMcBills <leokinyera9@gmail.com>
2023-06-11 20:52:20 +0300

    destroy world

---
+++ lambda/prac1.py
@@ -0,0 +1,17 @@
+# a function that destroys the world
+
+import time
+
+def destroy_world():
+    print("Destroying the world!")
+    time.sleep(5)
+    print("3...")
+    time.sleep(1)
+    print("2...")
+    time.sleep(1)
+    print("1...")
+    time.sleep(0.5)
+    print("BOOM!")
+
+
+destroy_world()
