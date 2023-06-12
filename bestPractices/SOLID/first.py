fabbec16f874882c844f822215215f967714842e
LeoMcBills <leokinyera9@gmail.com>
2023-06-12 13:40:55 +0300

    add documentation

---
+++ bestPractices/SOLID/first.py
@@ -0,0 +1,14 @@
+"""The practices directed in this directory are for best practices
+
+There's text given in context of the SOLID programming principals
+-----------------------------------------
+- Single Responsibility Principle (SRP)
+- Open-Closed Principle (OCP)
+- Liskov Substitution Principle (LSP)
+- Interface Segregation Principle (ISP)
+- Dependency Inversion Principle (DIP)
+-----------------------------------------
+- Please follow them for best practices
+"""
+
+print("Read the last point in the documentation and execute it")
