a0d7590c79430c68f2b55869c82aa02601887136
LeoMcBills <leokinyera9@gmail.com>
2023-06-14 13:28:15 +0300

    append text to greatest.txt

--- asa/filemanager.py
+++ asa/filemanager.py
@@ -3,5 +3,15 @@ file = open("Greatest.txt", "r")
 for line in file:
     print(line, end='')
 
+file.close()
+
+file = open("Greatest.txt", "a")
+
+file.write("\nThis actually works!")
+
+file = open("Greatest.txt", "r")
+
+for line in file:
+    print(line, end="")
 
 file.close()
