249c205ef0ed29825cd0e92845175bf378256665
LeoMcBills <leokinyera9@gmail.com>
2023-06-20 12:59:24 +0300

    error

--- leetcode/richestcustomerwealth.py
+++ leetcode/richestcustomerwealth.py
@@ -1,4 +1,13 @@
 rich = {"Elon Musk":"280M", "Jeff Benzos":"176M", "Bernault Ashault":"297M"}
 
+i = 0
+
 for person in rich.items():
-    print(person)
+   # print(person)
+    listThem = []
+    listThem[i] += person
+    i += 1
+    if i == 2:
+        break
+
+print(listThem)
