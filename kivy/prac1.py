f7b562785307b586d15e3b0db52e32f472e0cef6
LeoMcBills <leokinyera9@gmail.com>
2023-06-29 21:24:46 +0300

    add f;loatlayout

--- kivy/prac1.py
+++ kivy/prac1.py
@@ -5,6 +5,7 @@ kivy.require("2.2.1")
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.graphics import Rectangle, Color
+from kivy.uix.floatlayout import FloatLayout
 
 class HomeWidget(Widget):
     def __init__(self, **kwargs):
