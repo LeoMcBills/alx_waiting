bc49a4938bda32eda7fdede7903c809af7c0a9d7
LeoMcBills <leokinyera9@gmail.com>
2023-06-30 18:07:32 +0300

    consider all test cases

--- leetcode/indices.py
+++ leetcode/indices.py
@@ -1,11 +1,12 @@
-nums = [3, 2, 4]
+nums = [3, 3]
 target = 6
 indices = []
 
 for index, elem in enumerate(nums):
-    for num in nums:
+    for pos, num in enumerate(nums):
         oslo = nums[index]
-        if oslo == num:
+        print(pos)
+        if oslo == num and index == pos:
             continue
         num = oslo + num
         if num == target:
