9d3c5fc057722aa9b6bc8c5b30effc2f86ef2f60
LeoMcBills <leokinyera9@gmail.com>
2023-06-30 08:13:59 +0300

    add scatter

--- kivy/prac1.py
+++ kivy/prac1.py
@@ -6,6 +6,7 @@ from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.graphics import Rectangle, Color
 from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.scatterlayout import ScatterLayout
 
 class HomeWidget(Widget):
     def __init__(self, **kwargs):
