e93439414ed131e214b61345ba5bb977d618c923
LeoMcBills <leokinyera9@gmail.com>
2023-06-30 21:51:52 +0300

    failed

--- leetcode/substr.py
+++ leetcode/substr.py
@@ -2,7 +2,15 @@ s = 'pwwkew'
 a = [*s]
 substr = []
 generalList = []
+remover = []
 
-num = len(a)
+num = len(a) - 1
+for elem in range(num):
+    for pos, letter in enumerate(a):
+        if letter in substr:
+            break    
+        substr.append(letter)
+        a.pop(pos)
+    generalList.append(substr)
 print(num)
 print(generalList)
