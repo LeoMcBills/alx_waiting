312510d2819cb7e9106976d5e93d66da8d96106d
LeoMcBills <leokinyera9@gmail.com>
2023-07-05 20:33:36 +0300

    got a ZeroDivisionError

--- newFiles/file.py
+++ newFiles/file.py
@@ -6,6 +6,14 @@ except error:
     print("MATH ERROR")
 """
 
-number
+number = 1
 
 print(number * 3)
+
+print("Do the Try and Except thing")
+
+
+x = 12 / 0
+print("Wait how did you pass the exceptional checks")
+
+
