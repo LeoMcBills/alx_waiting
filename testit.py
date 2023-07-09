db4b42b67c800dd508fef0f0e9aeb016ddc27544
LeoMcBills <leokinyera9@gmail.com>
2023-07-09 15:18:45 +0300

    add testit.py to directory

---
+++ testit.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+def add(a, b):
+    """Sum of numbers a and b
+
+    Parameters
+    ----------
+    a -> int
+        first number
+    b -> int
+        second number
+
+    Returns
+    -------
+    a + b
+
+    >>> print(add(2, 3))
+    5
+    >>> print(add(4, 5))
+    9
+    """
+    return a + b
+
+print(add(2, 3))
