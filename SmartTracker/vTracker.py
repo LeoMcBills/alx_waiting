76916086b1e0e1f65dcd2bd65de3ada9f37204f6
LeoMcBills <leokinyera9@gmail.com>
2023-07-15 21:08:08 +0300

    app skeleton

--- SmartTracker/vTracker.py
+++ SmartTracker/vTracker.py
@@ -1,29 +1,34 @@
-# set kivy
-
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.uix.button import Button
-from kivy.uix.slider import Slider
 from kivy.uix.label import Label
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.popup import Popup
-from kivy.properties import NumericProperty, ReferenceListProperty, ObjectProperty
-from kivy.clock import Clock
-from kivy.graphics import Color, Ellipse, Line
-from kivy.core.window import Window
-from kivy.uix.floatlayout import FloatLayout
-from kivy.uix.image import Image
-from kivy.uix.behaviors import ButtonBehavior
-from kivy.uix.screenmanager import ScreenManager, Screen
-from kivy.uix.textinput import TextInput
-from kivy.uix.dropdown import DropDown
-from kivy.uix.spinner import Spinner
-from kivy.uix.checkbox import CheckBox
 
-# set python
-import os
-import sys
-import time
-import datetime
-import numpy as np
 
+class StartScreen(Widget):
+    pass
+
+class LoginScreen(Widget):
+    pass
+
+class RegisterScreen(Widget):
+    pass
+
+class MainScreen(Widget):
+    pass
+
+class ScanScreen(Widget):
+    pass
+
+class RegisterPatientScreen(Widget):
+    pass
+
+class PatientScreen(Widget):
+    pass
+
+class vTrackerApp(App):
+    def build(self):
+        return StartScreen()
+        
+    
+if __name__ == '__main__':
+    vTrackerApp().run()
