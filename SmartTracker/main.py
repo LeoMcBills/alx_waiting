28be10843014d6edef04639479a941d21ddfbd29
LeoMcBills <leokinyera9@gmail.com>
2023-07-15 21:53:16 +0300

    add kv lang file

--- SmartTracker/main.py
+++ SmartTracker/main.py
@@ -4,6 +4,7 @@ from kivy.uix.button import Button
 from kivy.uix.label import Label
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.boxlayout import BoxLayout
 
 class StartScreen(GridLayout):
     pass
