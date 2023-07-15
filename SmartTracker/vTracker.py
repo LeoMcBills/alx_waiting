e64ed3d4f7dda7545a21bd53a0db53f642a2813d
LeoMcBills <leokinyera9@gmail.com>
2023-07-15 19:54:57 +0300

    class project

---
+++ SmartTracker/vTracker.py
@@ -0,0 +1,29 @@
+# set kivy
+
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.uix.button import Button
+from kivy.uix.slider import Slider
+from kivy.uix.label import Label
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.popup import Popup
+from kivy.properties import NumericProperty, ReferenceListProperty, ObjectProperty
+from kivy.clock import Clock
+from kivy.graphics import Color, Ellipse, Line
+from kivy.core.window import Window
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.image import Image
+from kivy.uix.behaviors import ButtonBehavior
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.textinput import TextInput
+from kivy.uix.dropdown import DropDown
+from kivy.uix.spinner import Spinner
+from kivy.uix.checkbox import CheckBox
+
+# set python
+import os
+import sys
+import time
+import datetime
+import numpy as np
+
