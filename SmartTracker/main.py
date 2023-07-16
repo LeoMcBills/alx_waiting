4b2000d1ee51bd34a59be39b062d49d6f342307f
LeoMcBills <leokinyera9@gmail.com>
2023-07-16 16:07:20 +0300

    correct kv file loading error

--- SmartTracker/main.py
+++ SmartTracker/main.py
@@ -1,13 +1,21 @@
 from kivy.app import App
+from kivy.metrics import dp
 from kivy.uix.widget import Widget
 from kivy.uix.button import Button
 from kivy.uix.label import Label
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.boxlayout import BoxLayout
+from kivy.properties import BooleanProperty, StringProperty
 
 class StartScreen(GridLayout):
     pass
+#     def __init__(self, **kwargs):
+#         super().__init__(**kwargs)
+#         for i in range(0, 100):
+#             size = dp(100)
+#         b = Button(text=str(i+1), size_hint=(None, None), size=(size, size))
+#         self.add_widget(b)
 
 class LoginScreen(Widget):
     pass
@@ -28,6 +36,7 @@ class PatientScreen(Widget):
     pass
 
 class TrackerApp(App):
-    pass
+    kv_file = 'tracker.kv'
 
-TrackerApp().run()
+if __name__ == '__main__':
+    TrackerApp().run()
